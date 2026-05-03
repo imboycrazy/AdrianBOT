8a3436efad655267e94ccc9486ef1e9b7560ee9e
Tobis <swissinquiries13@gmail.com>
2026-05-03 19:21:46 +0200

    3 new responses to /adrian

--- responses.ts
+++ responses.ts
@@ -66,6 +66,9 @@ export const allResponses = [
   "omg i got this what does it mean https://cdn.discordapp.com/attachments/1008809187079376966/1489711892737036469/image.png?ex=69f45990&is=69f30810&hm=0fafa6586632d992be958869e87687d6bf610769e2e35f8db089886462a52ec8&",
   "ugh this incomming summer hit @lizzo ily [!](https://open.spotify.com/track/53OEjS6e0dH9PGgrkr0sH4?si=3b5533ba8d874a05)",
   "Excuse me queef, not to be disrespectful or rude but could you please take post down. That is my lawyer who was transitioned by a British chop writer. And it this post is very defamatory. Idk who you are or if you even know him but I need you to take this down please.",
+  "lizzo got me a birthday cake 🩷 https://cdn.discordapp.com/attachments/1350086261112700988/1500546995243323402/cake.mp4?ex=69f8d4ca&is=69f7834a&hm=bcc0c9ee8185e4f7c608e4721ac5775edf307e67819edf5aefb10214c6622741&",
+  "asked my bestie Lizzo to promote origami and she did it 🥹 https://cdn.discordapp.com/attachments/1350086261112700988/1500547386819481660/promo.mp4?ex=69f8d527&is=69f783a7&hm=0765fc35320fcc55cda560eca73e06cc4496f5b0c39c820fae0cb6b518d5c4db&",
+  "can yall rate my ass in the scale from 1 to 10? x https://cdn.discordapp.com/attachments/1350086261112700988/1500547748779393167/ass.mp4?ex=69f8d57e&is=69f783fe&hm=57f8d801c50e6dba5acdbf660af52f16c7238207ef566da92b04f9701b11fb4b&",
 ];
 
 export const dinnerAnswers = [
